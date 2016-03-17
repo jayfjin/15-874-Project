32d9eb48f38095ce812fb47e1dbb8ecbb74b5cb4
Jay Jin <jayjin80@gmail.com>
2016-03-17 17:42:23 -0400

    fixed noise add

--- mnist/dendrite_thres_noise.py
+++ mnist/dendrite_thres_noise.py
@@ -4,13 +4,13 @@ from tensorflow.examples.tutorials.mnist import input_data
 mnist = input_data.read_data_sets('MNIST_data', one_hot=True)
 
 #add noise
-mnist.train.images = map(lambda y: map(lambda x: x + np.random.normal(0,5), y)
-                         , mnist.train.images)
-mnist.test.images = map(lambda y: map(lambda x: x + np.random.normal(0,5), y)
-                         , mnist.train.images)
+mnist.train._images = map(lambda y: map(lambda x: x + np.random.normal(0,5), y)
+                         , mnist.train._images)
+mnist.test._images = map(lambda y: map(lambda x: x + np.random.normal(0,5), y)
+                         , mnist.train._images)
 
-mnist.validation.images = map(lambda y: map(lambda x: x + np.random.normal(0,5), y)
-                         , mnist.train.images)
+mnist.validation._images = map(lambda y: map(lambda x: x + np.random.normal(0,5), y)
+                         , mnist.train._images)
 
 import tensorflow as tf
 sess = tf.InteractiveSession()
