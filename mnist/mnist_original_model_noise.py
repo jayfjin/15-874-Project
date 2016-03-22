e9f1e3b57d361c95af9c51097757cec278148735
Jack Paparian <jackpaparian@gmail.com>
2016-03-22 00:15:58 -0400

    Used the skimage noise function

--- mnist/mnist_original_model_noise.py
+++ mnist/mnist_original_model_noise.py
@@ -1,19 +1,38 @@
-import numpy as np
-import multiprocessing
-import skimage
-import tensorflow as tf
-
 from tensorflow.examples.tutorials.mnist import input_data
 mnist = input_data.read_data_sets('MNIST_data', one_hot=True)
+
+import skimage
 
 mnist.train._images = skimage.util.random_noise(mnist.train._images, mode='gaussian', var=0.5)
 mnist.test._images = skimage.util.random_noise(mnist.test._images, mode='gaussian', var=0.5)
 mnist.validation._images = skimage.util.random_noise(mnist.validation._images, mode='gaussian', var=0.5)
 
+import tensorflow as tf
 sess = tf.InteractiveSession()
 
 x = tf.placeholder(tf.float32, shape=[None, 784])
 y_ = tf.placeholder(tf.float32, shape=[None, 10])
+
+W = tf.Variable(tf.zeros([784,10]))
+b = tf.Variable(tf.zeros([10]))
+
+sess.run(tf.initialize_all_variables())
+
+y = tf.nn.softmax(tf.matmul(x,W) + b)
+
+cross_entropy = -tf.reduce_sum(y_*tf.log(y))
+
+train_step = tf.train.GradientDescentOptimizer(0.01).minimize(cross_entropy)
+
+for i in range(1000):
+  batch = mnist.train.next_batch(50)
+  train_step.run(feed_dict={x: batch[0], y_: batch[1]})
+
+correct_prediction = tf.equal(tf.argmax(y,1), tf.argmax(y_,1))
+
+accuracy = tf.reduce_mean(tf.cast(correct_prediction, tf.float32))
+
+print(accuracy.eval(feed_dict={x: mnist.test.images, y_: mnist.test.labels}))
 
 def weight_variable(shape):
   initial = tf.truncated_normal(shape, stddev=0.1)
@@ -23,7 +42,6 @@ def bias_variable(shape):
   initial = tf.constant(0.1, shape=shape)
   return tf.Variable(initial)
 
-
 def conv2d(x, W):
   return tf.nn.conv2d(x, W, strides=[1, 1, 1, 1], padding='SAME')
 
@@ -32,24 +50,18 @@ def max_pool_2x2(x):
                         strides=[1, 2, 2, 1], padding='SAME')
 
 W_conv1 = weight_variable([5, 5, 1, 32])
-#b_conv1 = bias_variable([32])
-
-b_thresh1 = bias_variable([1,28,28,1])
+b_conv1 = bias_variable([32])
 
 x_image = tf.reshape(x, [-1,28,28,1])
 
-h_conv1 = conv2d(tf.nn.relu(x_image + b_thresh1), W_conv1)
+h_conv1 = tf.nn.relu(conv2d(x_image, W_conv1) + b_conv1)
 h_pool1 = max_pool_2x2(h_conv1)
 
-
 W_conv2 = weight_variable([5, 5, 32, 64])
-# b_conv2 = bias_variable([64])
+b_conv2 = bias_variable([64])
 
-b_thresh2 = bias_variable([1,14,14,32])
-
-h_conv2 = conv2d(tf.nn.relu(h_pool1 + b_thresh2), W_conv2)
+h_conv2 = tf.nn.relu(conv2d(h_pool1, W_conv2) + b_conv2)
 h_pool2 = max_pool_2x2(h_conv2)
-
 
 W_fc1 = weight_variable([7 * 7 * 64, 1024])
 b_fc1 = bias_variable([1024])
@@ -59,7 +71,6 @@ h_fc1 = tf.nn.relu(tf.matmul(h_pool2_flat, W_fc1) + b_fc1)
 
 keep_prob = tf.placeholder(tf.float32)
 h_fc1_drop = tf.nn.dropout(h_fc1, keep_prob)
-
 
 W_fc2 = weight_variable([1024, 10])
 b_fc2 = bias_variable([10])
@@ -81,6 +92,4 @@ for i in range(20000):
 
 print("test accuracy %g"%accuracy.eval(feed_dict={
     x: mnist.test.images, y_: mnist.test.labels, keep_prob: 1.0}))
-
-
 
