6e9769427ac546cc89405a6672b78e94a2461016
Jay Jin <jayjin80@gmail.com>
2016-04-17 16:06:23 -0400

    plot TSNE

---
+++ autoencoder/plot_TSNE.py
@@ -0,0 +1,18 @@
+import matplotlib.pyplot as plt
+import numpy as np
+import sys
+
+def plot_with_labels(low_dim_embs, labels, filename='tsne.png'):
+  assert low_dim_embs.shape[0] >= len(labels), "More labels than embeddings"
+  plt.figure(figsize=(18, 18))  #in inches
+  for i, label in enumerate(labels):
+    x, y = low_dim_embs[i,:]
+    print labels
+    plt.scatter(x, y, c = np.asarray(labels), cmap=plt.cm.coolwarm)
+    plt.savefig(filename)
+
+low_dim_embs = np.loadtxt('dae_tsne.csv', delimiter = ',')
+labels = np.loadtxt('dae_testY.csv', delimiter = ',')
+labels = map(lambda x: x.index(1), labels.tolist())
+plot_with_labels(low_dim_embs, labels)
+
